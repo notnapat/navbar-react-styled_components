9a743e5c62abca8f4bbb0ef9cf68d15d334427f7
notnapat <notnapat9@gmail.com>
2022-11-17 22:44:31 +0700

    เสร็จ มี 5.7 ขั้นตอน

--- navber/src/components/Navbar.js
+++ navber/src/components/Navbar.js
@@ -1,23 +1,73 @@
 // 2 rfce
-import React from 'react'
+// 5 useState
+import React, { useState } from 'react'
 // 3.2 NavbarContainer
 // 3.6 Left , RightContainer
+// import เข้ามา แล้วเอาไปใช้ใน function แต่บางที ดึงไปใช้ใน function แล้ว import มาให้ อัตโนมัติ
 import {
-      NavbarContainer,
-      LeftContainer,
-      RightContainer
+    NavbarContainer,
+    LeftContainer,
+    RightContainer,
+    NavbarInnerContainer,
+    NavbarExtendedContainer,
+    NavbarLinkContainer,
+    NavbarLink,
+    Logo,
+    OpenLinksButton,
+    NavbarLinkExtended,
 } from "../styles/Navbar.style"
+//4 logo = LogoImg สามารถตั้งเป็นชื่ออื่นได้ เหมือนตัวแปล
+import LogoImg from "../assets/Logo-Free.png"
 
 function Navbar() {
-      return (
-            // 3.3 Navbar
-            //3.7 Left, Right
-            <NavbarContainer>
-                  {""}
-                  <LeftContainer></LeftContainer>
-                  <RightContainer></RightContainer>
-            </NavbarContainer>
-      )
+    // 5.1 extend setState
+    const [extendNavbar, setExtendNavbar] = useState(false)
+    return (
+        // 3.3 Navbar
+        // 3.7 Left, Right
+        // 3.10 Inner
+        // 3.11 Extens
+        // 3.13 LinkCon
+        // 3.16 Link
+        // 4.2 Logo
+        // 4.4 OpenLinksButton  , &#8801 == คือ บล็อค 3 ขีด ให้แสดงในมือถือ
+        // 5.2 onClick even , extendNav states ? <>
+        // 5.3 copy NavbarLink  to NabarExtendedCon
+        // 5.4 extendNavbar &&
+        // 5.5 extendNavbar={extendNavbar} in NavbarContainer
+        // 5.8 แก้ใน {externNavbar && >> แก้ NavbarLink เป็น NavbarLinkExtended
+
+        <NavbarContainer extendNavbar={extendNavbar}>
+            <NavbarInnerContainer  >
+                <LeftContainer>
+                    <NavbarLinkContainer>
+                        <NavbarLink to="/">Home</NavbarLink>
+                        <NavbarLink to="/products">Products</NavbarLink>
+                        <NavbarLink to="/contact">Contact</NavbarLink>
+                        <NavbarLink to="/about">About</NavbarLink>
+                        <OpenLinksButton
+                            onClick={() => {
+                                setExtendNavbar((curr) => !curr)
+                            }}
+                        >
+                            {extendNavbar ? <>&#10005;</> : <>&#8801;</>}
+                        </OpenLinksButton>
+                    </NavbarLinkContainer>
+                </LeftContainer>
+                <RightContainer>
+                    <Logo src={LogoImg}></Logo>
+                </RightContainer>
+            </NavbarInnerContainer>
+            {extendNavbar && (
+                <NavbarExtendedContainer>
+                    <NavbarLinkExtended to="/">Home</NavbarLinkExtended>
+                    <NavbarLinkExtended to="/products">Products</NavbarLinkExtended>
+                    <NavbarLinkExtended to="/contact">Contact</NavbarLinkExtended>
+                    <NavbarLinkExtended to="/about">About</NavbarLinkExtended>
+                </NavbarExtendedContainer>
+            )}
+        </NavbarContainer >
+    )
 }
 
 export default Navbar
--- navber/src/styles/Navbar.style.js
+++ navber/src/styles/Navbar.style.js
@@ -1,13 +1,26 @@
 // 3
 import styled from "styled-components";
+// 3.14 Link
+import { Link } from "react-router-dom";
+
 
 //3.1
+// ?5.6?
 export const NavbarContainer = styled.nav`
-width: 100;
-height: 80px;
+width: 100%;
+height: ${(props) => (props.extendNavbar ? "100vh" : "80px")}; /* vh == %*/
 background-color: black;
 display: flex;
 flex-direction: column;
+
+@media (min-width: 701px){
+    height: 80px;
+}
+/* width: 100;
+height: 80px;
+background-color: black;
+display: flex;
+flex-direction: column; */
 `;
 //3.3
 export const LeftContainer = styled.div`
@@ -21,7 +34,76 @@ background-color: red;
 export const RightContainer = styled.div`
 flex: 30%;
 display: flex;
+justify-content: flex-end;
+padding-right:50px;
+/* flex: 30%;
+display: flex;
 align-items: center;
 padding-left: 50px;
-background-color: salmon;
+background-color: salmon; */
 `
+
+//3.8
+export const NavbarInnerContainer = styled.div`
+width:100%;
+height:80px;
+display: flex;
+`
+//3.9
+// ?5.6? style to NavbarExten 
+export const NavbarExtendedContainer = styled.div`
+display: flex;
+flex-direction: column;
+align-items: center;
+
+@media (min-width: 701px){
+    display: none;
+}
+`;
+//3.12
+export const NavbarLinkContainer = styled.div`
+display:flex;
+`
+//3.15 
+//4.5 @media
+export const NavbarLink = styled(Link)`
+color:white;
+font-size:x-large;
+font-family: Arial, Helvetica, sans-serif;
+text-decoration:none;
+margin:10px;
+
+@media (max-width: 700px) {
+    display:  none;
+}
+`
+// 5.7
+export const NavbarLinkExtended = styled(Link)`
+color:white;
+font-size:x-large;
+font-family: Arial, Helvetica, sans-serif;
+text-decoration:none;
+margin:10px;
+`
+//4.1
+export const Logo = styled.img`
+margin:10px;
+max-width:180px;
+height: auto;
+`
+//4.3
+//4.6 @media
+export const OpenLinksButton = styled.button`
+width:70px;
+height:50px;
+background:none;
+border:none;
+color:white;
+font-size:45px;
+cursor: pointer;
+
+@media (min-width: 701px) {
+    display:  none;
+}
+`
+
